4c1e1fef85025f500a1f242b6b41136100fb1c88
Kakitsubun <ka.kitsubun@donuts.ne.jp>
2018-12-17 15:35:03 +0900

    汎用テンプレート枠の作成
    make VIEW_NAME=base create/view
    make VIEW_NAME=header create/view
    make VIEW_NAME=footer create/view

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/gacha">Gacha</a>
+            <a class="navbar-brand" href="/player-gacha">PlayerGacha</a>
+        </div>
+    </div>
+</nav>
