9746779d42502cc0f489a1c8c3fd79ce84c51f08
kakitsubun <kakitsubun@gmail.com>
2019-02-06 10:44:03 +0900

    Windows対応

--- init.sh
+++ init.sh
@@ -1,21 +1,20 @@
-#!/bin/sh
-chmod -R 777 storage
+#!/bin/sh 
+chmod -R 777 storage 
 
-if [ ! -f ".env" ] 
-then
+if [ ! -f ".env" ]
+then 
     if [ -d "vendor" ] 
-    then
-        rm -rf vendor
-    fi
-    mkdir vendor
-    chmod -R 777 vendor
-    composer install
-    cp -f env.local .env
-    php artisan key:generate
-fi
+    then 
+        rm -rf vendor 
+    fi 
+    mkdir vendor 
+    chmod -R 777 vendor 
+    composer install 
+    cp -f env.local .env 
+    php artisan key:generate 
+fi 
 
-composer update
+composer update 
 php artisan migrate
-
-# start php-fpm
-/usr/local/sbin/php-fpm
+# start php-fpm 
+/usr/local/sbin/php-fpm 
