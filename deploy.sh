a590a996811ce77da7515265d88f2b39c1ace6af
kakitsubun <kakitsubun@gmail.com>
2019-02-08 18:08:59 +0900

    Tuning

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,17 @@ then
     rm -rf vendor
 fi
 mkdir vendor
-chmod -R 777 vendor
-chmod -R 777 storage
+chmod -R 755 storage
+chmod -R 755 vendor
 
 composer install
 php artisan migrate --force
 php artisan view:clear
+
+# Change owner to mode for next deploy
+USERNAME=$(whoami)
+chmod -R 755 ./
+chown -R $USERNAME:$USERNAME ./
 
 ## cp
 #npm dev
