7f83a510e10d6a6161c13abf47cf95dc6d1bfee7
kakitsubun <kakitsubun@gmail.com>
2019-02-18 14:56:00 +0900

    Deploy Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hana Gacha Test
+                    Hana Gacha Test 2
                 </div>
 
                 <div class="links">
