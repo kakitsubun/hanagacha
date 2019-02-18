1c3e93b94c37b87a09e0cc459a36b406db3ce0c2
kakitsubun <kakitsubun@gmail.com>
2019-02-18 16:40:40 +0900

    CI-TEST 最後1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hana Gacha(CI-TEST)
+                    Hana Gacha
                 </div>
 
                 <div class="links">
