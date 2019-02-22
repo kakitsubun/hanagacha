f738d40603d78285f7dc02798308a787fdf12375
kakitsubun <kakitsubun@gmail.com>
2019-02-22 18:03:54 +0900

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Deployer Success !!!!!!!!!!!!
+                    Deployer Success !!!!!!
                 </div>
 
                 <div class="links">
