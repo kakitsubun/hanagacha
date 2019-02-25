3ed5700a99c9ace84132aa7f9999d0b846fbc6ae
kakitsubun <kakitsubun@gmail.com>
2019-02-25 16:25:49 +0900

    Bug Fix

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo rm -rf /var/www/html/web
-git clone https://github.com/kakitsubun/hanagacha.git /var/www/html/web
+sudo rm -rf /var/www/html/workspace
+git clone https://github.com/kakitsubun/hanagacha.git /var/www/html/workspace
 
